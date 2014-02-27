262329276d13a2230e85bbb236e6ff224bb26378
Anirudh Sanjeev <anirudh@razorflow.com>
2014-02-27 12:19:19 +0530

    made tests fail. yay!

--- tests/inception.js
+++ tests/inception.js
@@ -1,7 +1,21 @@
-require(["core/main"], function(main) {
-	describe('inception', function() {
-    	it('window should have rf object', function() {
-      	  	expect(window.rf).toBeDefined();
+require(["prop/propertybase"], function(PropertyBase) {
+
+	function Level1Props () {
+        PropertyBase.call (this);
+
+        this.register ({
+            foo: null,
+            bar: 42
+        })
+    }
+	describe('PropertyBase tests', function() {
+    	it('should have default values to data', function() {
+      		var prop = new Level1Props();
+
+      		expect(prop.getValue("foo")).toBe(null);
+      		expect(prop.getValue("bar")).toBe(42);
     	});
 	});
+
+	window.jasmineEnv.execute();
 });
