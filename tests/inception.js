08f9bc036b9d6e8bf05d2d45a97ada82397f49c9
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2014-02-27 11:44:56 +0530

    Added the testing architecture

---
+++ tests/inception.js
@@ -0,0 +1,7 @@
+require(["core/main"], function(main) {
+	describe('inception', function() {
+    	it('window should have rf object', function() {
+      	  	expect(window.rf).toBeDefined();
+    	});
+	});
+});
--- tools/devserver/index.js
+++ tools/devserver/index.js
@@ -5,12 +5,19 @@ var path = require('path');
 var rootPath = path.resolve(__dirname, '../../');
 
 app.use('/assets', express.static(rootPath + '/src'));
+app.use('/testScripts', express.static(rootPath + '/tests'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
     var vendorScripts = fs.readdirSync(rootPath + '/src/vendor/js');
     res.render('index.ejs', {vendorScripts: vendorScripts});
+});
+
+app.get('/tests', function(req, res) {
+    var vendorScripts = fs.readdirSync(rootPath + '/src/vendor/js');
+    var testScripts = fs.readdirSync(rootPath + '/tests');
+    res.render('tests.ejs', {vendorScripts: vendorScripts, testScripts: testScripts});
 });
 
 app.listen(9090);
