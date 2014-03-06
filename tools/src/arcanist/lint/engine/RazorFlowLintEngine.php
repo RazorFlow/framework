50e0e6cb6b1c90b967f0cfe37e1c62eab528e6ff
Anirudh Sanjeev <anirudh@razorflow.com>
2014-03-06 16:22:40 +0530

    minor typo in arc lint

--- tools/src/arcanist/lint/engine/RazorFlowLintEngine.php
+++ tools/src/arcanist/lint/engine/RazorFlowLintEngine.php
@@ -15,7 +15,7 @@ final class RazorFlowLintEngine extends ArcanistLintEngine {
       }
     }
 
-    $linters[] = id(new ArcanistTextLinter())->setPaths($text_paths);
+    $linters[] = id(new ArcanistTextLinter())->setPaths($text);
     $linters[] = id(new ArcanistJSHintLinter())
                     ->setPaths(preg_grep('/\.js$/', $paths));
 
