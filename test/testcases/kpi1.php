bea9f9fac12b17a7072fba93754852ba0e79b8d9
Anirudh Sanjeev <anirudh@razorflow.com>
2014-03-12 18:26:28 +0530

    ported some basic kpi functions

--- test/testcases/kpi1.php
+++ test/testcases/kpi1.php
@@ -5,6 +5,9 @@ require "../../src/razorflow.php";
 class SampleDashboard extends StandaloneDashboard {
     public function initialize () {
         $kpi = new KPIComponent("foo");
+        $kpi->setCaption("Hello world");
+        $kpi->setValue(42);
+        $kpi->setDimensions(4, 4);
         $this->addComponent ($kpi);
     }
 }
@@ -12,5 +15,4 @@ class SampleDashboard extends StandaloneDashboard {
 $db = new SampleDashboard();
 $db->setStaticRoot("http://localhost:9090");
 $db->renderStandalone();
-
 
