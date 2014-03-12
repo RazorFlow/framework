ee735cadb696bf9a89b6055d78c47b494b9e4f5e
Anirudh Sanjeev <anirudh@razorflow.com>
2014-03-12 20:35:07 +0530

    initial work on events

--- test/testcases/kpi1.php
+++ test/testcases/kpi1.php
@@ -10,15 +10,27 @@ class SampleDashboard extends StandaloneDashboard {
         $kpi->setDimensions(4, 4);
         $this->addComponent ($kpi);
 
-        $table = new TableComponent('table1');
-        $table->setCaption("Table 1");
-        $table->setDimensions (6,6);
-        $table->addColumn('colA', "Column A");
-        $table->addColumn('colB', "Column B");
-        for ($i = 0; $i < 10; $i ++) {
-            $table->addRow(array('colA' => $i * 2, 'colB' => $i * 2 + 1));
-        }
-        $this->addComponent ($table);
+        // $table = new TableComponent('table1');
+        // $table->setCaption("Table 1");
+        // $table->setDimensions (6,6);
+        // $table->addColumn('colA', "Column A");
+        // $table->addColumn('colB', "Column B");
+        // for ($i = 0; $i < 10; $i ++) {
+        //     $table->addRow(array('colA' => $i * 2, 'colB' => $i * 2 + 1));
+        // }
+        // $this->addComponent ($table);
+
+        $kpi2 = new KPIComponent("kpi2");
+        $kpi2->setCaption("KPI 2");
+        $kpi2->setValue(45);
+        $kpi2->setDimensions(4, 4);
+        $this->addComponent ($kpi2);
+
+        $kpi->bindToEvent ("kpiClick", array($kpi2), "handleKPIClick");
+    }
+
+    public function handleKPIClick ($source, $target, $params) {
+        die ("Handling kpi click");
     }
 }
 
