bf5b868d951a092fbdfae783b737ecac61d4a488
Joe Abhsihek <joeabhishek@gmail.com>
2014-03-12 18:26:28 +0530

    Basic Version of ThemeGen created.

--- tools/src/themebuilder/tgTest.js
+++ tools/src/themebuilder/tgTest.js
@@ -1,4 +1,42 @@
-var themegen = require("./themegen");
-var less = require("less");
+var readline = require('readline');
+ 	lineReader = require('line-reader'),
+ 	stream = require('stream'),
+ 	themegen = require("./themegen"),
+ 	less = require("less"),
+			 themeObject = {
+			"@table-head-font-size":"15px",
+			"@table-body-color":"#f00"
+			},
+	sourceObject = "",
+	defaultVars = {},
+	fs = require('fs'),
+	source_file = __dirname + "/../../../src/less/theme/default.less",
+	default_theme_less = __dirname + "/../../../src/less/theme.less",
+	mixin_less = __dirname + "/../../../src/less/mixins.less";
 
-console.log(themegen.generateTheme ({}, "", less));
+	fs.readFile(default_theme_less, function (err, data) {
+	  	if (err){
+	  		console.log(err)
+	  	}else{
+	  		var default_less = data
+	  		fs.readFile(mixin_less,function(err,data){
+	  			if(err){
+	  				console.log(err)
+	  			}else{
+	  				default_mixin = data;
+	  				lineReader.eachLine(source_file, function(line, last) {
+						var x = line.split(":");
+					    var key = x[0];
+					    if(key[0] !== undefined) {
+					    	defaultVars[key] = x[1];
+					    }
+					    if(last){
+					    	themegen.generateTheme(themeObject, defaultVars, default_less + default_mixin, less, function (css) {
+								console.log(css);
+							})
+					    }
+					});
+	  			}
+	  		})	
+	  	}
+	});
--- tools/src/themebuilder/themegen.js
+++ tools/src/themebuilder/themegen.js
@@ -1,11 +1,35 @@
 (function() {
 	var themegen = {
-		generateTheme: function (themeObject, themeLessSourceCode, less) {
-			var parser = new less.Parser();
-
-			// TODO: all the processing
-
-			return "Hello World";
+		generateTheme: function (themeObject, defaultVariables, themeLessSourceCode, less, callback) {
+			for(var key in defaultVariables){
+				if(typeof(themeObject[key]) !== 'undefined'){
+					defaultVariables[key] = themeObject[key] + ';'
+				}
+			}
+			var newVariables = defaultVariables,
+				finalLess = themegen.convertObjectToVariablesLess(newVariables) + '\n' + themeLessSourceCode;
+			less.render(finalLess, function(err, css) {
+				if(err){
+					console.log(err)
+				}else{
+					callback(css)
+				}
+			});
+		},
+		convertObjectToVariablesLess: function (newVariables) {
+			var variables = '',
+				count = '';
+			data = newVariables;
+			for(var key in data){
+				if(data.hasOwnProperty(key)){
+					variables += key+':'+data[key]
+					count ++ ;
+					if(count<=Object.keys(data).length-1){
+						variables += '\n'
+					}
+				}
+			}
+			return variables
 		}
 	};
 
