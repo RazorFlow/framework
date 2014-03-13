a554d9a3685fa06070384aee906067f8c8c0b108
Swaroop SM <swaroop.striker@gmail.com>
2014-03-13 15:11:31 +0530

    added few other table component public methods

--- test/testcases/table.php
+++ test/testcases/table.php
@@ -8,14 +8,24 @@ class SampleDashboard extends StandaloneDashboard {
         $table = new TableComponent('table1');
         $table->setCaption("Table 1");
         $table->setDimensions (6,6);
-        $table->setRowsPerPage (3);
+        $table->setRowsPerPage (8);
         $table->addColumn('colA', "Column A");
         $table->addColumn('colB', "Column B");
+
         for ($i = 0; $i < 10; $i ++) {
+            // Single Row
             $table->addRow(array('colA' => $i * 2, 'colB' => $i * 2 + 1));
         }
-        $this->addComponent ($table);
 
+        // Multiple Rows
+        $rows = array(
+          array('colA' => 111, 'colB' => 22),
+          array('colA' => 222, 'colB' => 33),
+          array('colA' => 333, 'colB' => 44)
+        );
+        $table->addMultipleRows($rows);
+
+        $this->addComponent ($table);
     }
 }
 
