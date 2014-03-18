c0f2c30bacec7eb19389652c125828b945c7dac3
Anirudh Sanjeev <anirudh@razorflow.com>
2014-03-18 19:12:07 +0530

    added a build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,61 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         requirejs: {
-            compile: {
-                options: {
-                    baseUrl: "src/js",
-                    name: 'buildutils/almond',
-                    include: ['core/main'],
-                    insertRequire: ['core/main'],
-                    wrap: {
-                        'startFile': 'src/js/buildutils/start.js',
-                        'endFile': 'src/js/buildutils/end.js'
-                    }, 
-                    paths: {
-                        lodash: '../vendor/js/lodash.min',
-                        zepto: '../vendor/js/zepto.min',
-                        d3: '../vendor/js/d3.rf',
-                        Modernizr: '../vendor/js/modernizr.rf',
-                        spin: '../vendor/js/spin'
-                    },
-                    shim: {
-                        lodash: {
-                            exports: '_'
+                core: {
+                    options: {
+                        baseUrl: "src/js",
+                        name: 'buildutils/almond',
+                        include: ['core/main'],
+                        insertRequire: ['core/main'],
+                        wrap: {
+                            'startFile': 'src/js/buildutils/start.js',
+                            'endFile': 'src/js/buildutils/end.js'
+                        }, 
+                        paths: {
+                            lodash: '../vendor/js/lodash.rf.min',
+                            zepto: '../vendor/js/zepto.min',
+                            d3: '../vendor/js/d3.rf',
+                            Modernizr: '../vendor/js/modernizr.rf',
+                            spin: '../vendor/js/spin'
                         },
-                        zepto: {
-                            exports: '$'
+                        shim: {
+                            lodash: {
+                                exports: '_'
+                            },
+                            zepto: {
+                                exports: '$'
+                            },
+                            d3: {
+                                exports: 'd3'
+                            },
+                            Modernizr: {
+                                exports: 'Modernizr'
+                            },
+                            spin: {
+                                exports: 'Spinner'
+                            }
                         },
-                        d3: {
-                            exports: 'd3'
-                        },
-                        Modernizr: {
-                            exports: 'Modernizr'
-                        },
-                        spin: {
-                            exports: 'Spinner'
-                        }
-                    },
-                    out: "build/rjsoutput.js"
+                        out: "build/js/razorflow.min.js"
+                    }
                 }
-            }
         },
         concat: {
 
         },
         jst: {
-            compile: {
-                options: {
-                    prettify: true,
-                    processName: function (filePath) {
-                        var parts = filePath.split('/');
-                        var fileName = parts[parts.length - 1];
-                        return fileName.split('.')[0];
+            templates: {
+                compile: {
+                    options: {
+                        prettify: true,
+                        processName: function (filePath) {
+                            var parts = filePath.split('/');
+                            var fileName = parts[parts.length - 1];
+                            return fileName.split('.')[0];
+                        }
+                    },
+                    files: {
+                        "build/js/templates.js": ["src/templates/*.html"]
                     }
-                },
-                files: {
-                    "build/js/templates.js": ["src/templates/*.html"]
                 }
             }
         },
@@ -106,8 +108,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-lodash');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
 
     
-    grunt.registerTask('compile', ['less', 'jst']);
+    grunt.registerTask('compile', ['less', 'jst:templates']);
     grunt.registerTask('test', ['compile', 'karma:dev', 'shell:coverageReport']);
+
+    grunt.registerTask('build', ["less", "jst:templates", 'requirejs:core'])
 }
