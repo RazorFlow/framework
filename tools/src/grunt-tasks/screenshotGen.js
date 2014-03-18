44c8b9d67c34a056685f1ec02feea6788ff46240
Anirudh Sanjeev <anirudh@razorflow.com>
2014-03-18 21:07:58 +0530

    initial work on screenshot gen task for grunt

---
+++ tools/src/grunt-tasks/screenshotGen.js
@@ -0,0 +1,22 @@
+var _ = require('underscore');
+var fs = require('fs');
+var path = require ('path');
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerTask('screenshotGen', 'generate screenshots', function() {
+    var out = "window.rfDemosSquashed = {\n";
+    grunt.log.writeln("taking screenshots...");
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+    });
+
+    var files = grunt.file.expand(options.demos);
+
+    grunt.log.ok("Finished squashing.");
+  });
+
+};
