3157fe975c478c35c97daaf813562d221cb66119
Anirudh Sanjeev <anirudh@razorflow.com>
2014-03-18 23:23:38 +0530

    improvements to devserver and brought in examples folder

--- tools/src/devserver/index.js
+++ tools/src/devserver/index.js
@@ -4,6 +4,7 @@ var app = new express();
 var path = require('path');
 var rootPath = path.resolve(__dirname, '../../../');
 var _ = require('underscore');
+var glob = require("glob");
 
 app.use('/assets', express.static(rootPath + '/src'));
 app.use('/transfer', express.static(rootPath + '/src/dev/transfer'));
@@ -12,81 +13,42 @@ app.use('/testScripts', express.static(rootPath + '/tests'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+var samples = glob.sync(rootPath + "/src/samples/**/*.js");
+var sampleNames = [];
+var sampleLookup = {};
+var indexedSamples = {
+    'Demos': [],
+    'TestCases': [],
+    'Examples': []
+};
+for(var i = 0; i < samples.length; i++) {
+    var name = path.basename (samples[i], ".js");
+    sampleLookup[name] = samples[i];
+    sampleNames.push (name);
+
+    if(samples[i].indexOf("/demos/") !== -1) {
+        indexedSamples["Demos"].push(name);
+    }
+
+    if(samples[i].indexOf("/testcases/") !== -1) {
+        indexedSamples["TestCases"].push(name);
+    }
+
+    if(samples[i].indexOf("/examples/") !== -1) {
+        indexedSamples["Examples"].push(name);
+    }
+}
+
 app.get('/', function(req, res) {
-    var demoFiles = fs.readdirSync(rootPath + '/src/dev/demos');
-    var demos = _.map(demoFiles, function(file) {
-        return file.replace('.js', '');
-    });
 
-    var testcaseFiles = fs.readdirSync(rootPath + '/src/dev/testcases');
-    var testcases = _.map(testcaseFiles, function(file) {
-        return file.replace('.js', '');
-    });
-
-    res.render('index.ejs', {demos: demos, testcases: testcases});
+    res.render('index.ejs', {samples: samples, sampleNames: sampleNames, indexedSamples: indexedSamples});
 });
 
-app.get('/demos/:id', function(req, res) {
-    var vendorScripts = fs.readdirSync(rootPath + '/src/vendor/js');
-    var demoFiles = fs.readdirSync(rootPath + '/src/dev/demos');
-    var id = req.params.id;    
-    var demos = _.map(demoFiles, function(file) {
-        return file.replace('.js', '');
-    });
-    var buildFlag = req.query.hasOwnProperty('build');
-    if(req.params.id) {
-        if(demos.indexOf(id) !== -1) {
-            var file = fs.readFileSync(rootPath + '/src/dev/demos/' + id + '.js', 'utf-8');
-            res.render('demo.ejs', {vendorScripts: vendorScripts, file: file, build: buildFlag});
-        } else {
-            res.end('404 not found!');
-        }        
-    } 
-});
-
-app.get('/demos', function(req, res) {
-    var demoFiles = fs.readdirSync(rootPath + '/src/dev/demos');
-    var demos = _.map(demoFiles, function(file) {
-        return file.replace('.js', '');
-    });
-
-    res.render('demo_index.ejs', {files: demos});
-});
-
-app.get('/testcases/:id', function(req, res) {
-    var vendorScripts = fs.readdirSync(rootPath + '/src/vendor/js');
-    var demoFiles = fs.readdirSync(rootPath + '/src/dev/testcases');
+app.get('/dev/:id', function (req, res) {
     var id = req.params.id;
-    var demos = _.map(demoFiles, function(file) {
-        return file.replace('.js', '');
-    });
-    var buildFlag = req.query.hasOwnProperty('build');
-    if(req.params.id) {
-        if(demos.indexOf(id) !== -1) {
-            var file = fs.readFileSync(rootPath + '/src/dev/testcases/' + id + '.js', 'utf-8');
-            res.render('demo.ejs', {vendorScripts: vendorScripts, file: file, build: buildFlag});
-        } else {
-            res.end('404 not found!');
-        }        
-    } 
+    var file = fs.readFileSync(sampleLookup[id], 'utf-8');
+    res.render('demo.ejs', {file: file, build: false});
 });
-
-app.get('/testcases', function(req, res) {
-    var demoFiles = fs.readdirSync(rootPath + '/src/dev/testcases');
-    var demos = _.map(demoFiles, function(file) {
-        return file.replace('.js', '');
-    });
-
-    res.render('testcases_index.ejs', {files: demos});
-});
-
-app.get('/dev', function(req, res) {
-    var vendorScripts = fs.readdirSync(rootPath + '/src/vendor/js');
-    res.render('dev.ejs', {vendorScripts: vendorScripts});
-});
-
-
-
 
 app.get('/tests', function(req, res) {
     var vendorScripts = fs.readdirSync(rootPath + '/src/vendor/js');
