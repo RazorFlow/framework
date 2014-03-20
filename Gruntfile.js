9ae18a7a1f662dbd654821991f6dd3a5e45ff5a7
Anirudh Sanjeev <anirudh@razorflow.com>
2014-03-20 19:34:09 +0530

    changed gruntfile with new syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,63 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        shell: {
-            local_js_articles: {
-                command: 'node tools/razordoc/bin/app.js --configFile src/config/local_js_articles.json',
+        razordoc: {
+            local: {
                 options: {
-                    stdout: true
-                }
-            },
-            local_php_articles: {
-                command: 'node tools/razordoc/bin/app.js --configFile src/config/local_php_articles.json',
-                options: {
-                    stdout: true
-                }
-            },
-            local_jsapi: {
-                command: 'node tools/razordoc/bin/app.js --configFile src/config/local_js_api.json',
-                options: {
-                    stdout: true
-                }
-            },
-            local_phpapi: {
-                command: 'node tools/razordoc/bin/app.js --configFile src/config/local_php_api.json',
-                options: {
-                    stdout: true
-                }
-            },
-            copy_images: {
-                command: 'cp -r src/examples build/examples && cp -r src/exampleImages build/exampleImages',
-                options: {
-                    stdout: true
-                }
-            },
-            web_js_articles: {
-                command: 'node tools/razordoc/bin/app.js --configFile src/config/web_js_articles.json',
-                options: {
-                    stdout: true
-                }
-            },
-            web_js_api: {
-                command: 'node tools/razordoc/bin/app.js --configFile src/config/web_js_api.json',
-                options: {
-                    stdout: true,
-                    stderr: true
-                }
-            },
-            web_php_articles: {
-                command: 'node tools/razordoc/bin/app.js --configFile src/config/web_php_articles.json',
-                options: {
-                    stdout: true
-                }
-            },
-            web_php_api: {
-                command: 'node tools/razordoc/bin/app.js --configFile src/config/web_php_api.json',
-                options: {
-                    stdout: true
+                    articles: {
+                        "js": {
+                            src: "src/content/js/",
+                            relativeLinkPath: "js/"
+                        },
+                        "php": {
+                            src: "src/content/php/",
+                            relativeLinkPath: "php/"
+                        },
+                    },
+                    examples: {
+                        'js': {
+                            src: "src/examples/src/js/",
+                            srcSuffix: ".js",
+                            images: "src/examples/images/js/",
+                            imageSuffix: ".png",
+                            thumbs: "src/exampels/thumbs/js/",
+                            thumbSuffix: ".png",
+                            livePrefix: "http://sample.razorflow.com/php/",
+                            liveSuffix: ""
+                        },
+                        'php': {
+                            src: "src/examples/src/php/",
+                            srcSuffix: ".js",
+                            images: "src/examples/images/php/",
+                            imageSuffix: ".png",
+                            thumbs: "src/exampels/thumbs/php/",
+                            thumbSuffix: ".png",
+                            livePrefix: "http://sample.razorflow.com/php/",
+                            liveSuffix: ""
+                        }
+                    },
+                    api: {
+                        "js": {
+                            src: "../jsrf/src/js/**/*.js",
+                            out: "build/docs/js/api",
+                            relativeLinkPath: "js/api/"
+                        },
+                        "php": {
+                            src: "../phprf/src/lib/**/*.php",
+                            out: "build/docs/php/api",
+                            relativeLinkPath: "php/api/"
+                        }
+                    },
+                    linkPrefix: "/docs/",
+                    out: "build/docs",
+                    suffix: "html",
+                    /*
+                    base.ejs
+                    article.ejs
+                    api.ejs
+
+                    ... all other remaining EJS files
+                     */
+                    template: "src/templates/default"
                 }
             }
         },
@@ -67,9 +71,11 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadTasks("../razordoc/src/");
+
 
     
-    grunt.registerTask('compile', ['shell:local_js_articles', 'shell:local_php_articles', 'shell:local_jsapi', 'shell:local_phpapi', 'shell:copy_images']);
-    grunt.registerTask('webArticles', ['shell:web_js_articles', 'shell:web_php_articles']);
-    grunt.registerTask('webApi', ['shell:web_js_api', 'shell:web_php_api']);
+    // grunt.registerTask('compile', ['shell:local_js_articles', 'shell:local_php_articles', 'shell:local_jsapi', 'shell:local_phpapi', 'shell:copy_images']);
+    // grunt.registerTask('webArticles', ['shell:web_js_articles', 'shell:web_php_articles']);
+    // grunt.registerTask('webApi', ['shell:web_js_api', 'shell:web_php_api']);
 }
