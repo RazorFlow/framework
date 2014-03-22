17166d2d12b8d4f97964df3eaf45467478939965
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2014-03-22 19:03:31 +0530

    Website docs grunt config added

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,60 @@ module.exports = function (grunt) {
                      */
                     template: "src/templates/default/article_layout",
                     apiTemplates: "src/templates/default"
-                }    
+                }
+            },
+            website: {
+                options: {
+                    articles: {
+                        root: 'src/content'
+                    },
+                    examples: {
+                        'js': {
+                            src: "src/examples/js/",
+                            srcSuffix: ".js",
+                            imagePrefix: "http://samples.razorflow.com/_assets/images/js/",
+                            imageSuffix: ".png",
+                            thumbPrefix: "http://samples.razorflow.com/_assets/images/js/thumbs/",
+                            thumbSuffix: ".png",
+                            livePrefix: "http://samples.razorflow.com/js/",
+                            liveSuffix: ".html"
+                        },
+                        'php': {
+                            src: "src/examples/php/",
+                            srcSuffix: ".js",
+                            imagePrefix: "http://samples.razorflow.com/_assets/images/php/",
+                            imageSuffix: ".png",
+                            thumbPrefix: "http://samples.razorflow.com/_assets/images/php/thumbs/",
+                            thumbSuffix: ".png",
+                            livePrefix: "http://samples.razorflow.com/php/",
+                            liveSuffix: ".html"
+                        }
+                    },
+                    api: {
+                        "js": {
+                            src: ["../jsrf/src/js/components/*.js"],
+                            out: "build/docs/api/js",
+                            relativeLinkPath: "api/js/"
+                        },
+                        "php": {
+                            src: ["../phprf/src/lib/components/*.php"],
+                            out: "build/docs/api/php",
+                            relativeLinkPath: "api/php/"
+                        }
+                    },
+                    linkPrefix: "",
+                    out: "../webrf/backend/docs",
+                    suffix: "html",
+                    /*
+                    base.ejs
+                    article.ejs
+                    api.ejs
+
+                    ... all other remaining EJS files
+                     */
+                    template: "src/templates/razorflow_dotcom/article_layout",
+                    apiTemplates: "src/templates/razorflow_dotcom"
+                }
             }
         },
         watch: {
