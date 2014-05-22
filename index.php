b26e187a11b798a956248e905de2525423d3a302
Anirudh Sanjeev <anirudh@razorflow.com>
2014-05-22 11:54:12 +0530

    enable custom items

--- index.php
+++ index.php
@@ -6,5 +6,10 @@ if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return false;
 }
 
+if(substr($_SERVER['REQUEST_URI'], 0, 4) === "/app") {
+	return false;
+}
+
+
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/internal/index.php';
