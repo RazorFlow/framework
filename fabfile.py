1ad3f08be50e78528b5f9d740d927c1bd59351a4
narendrashetty <narendra@razorflow.com>
2014-06-18 13:15:52 +0530

    added fabfile for custombuilder

---
+++ fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import *
+from fabric.colors import green, yellow, red
+from fabric.contrib.project import rsync_project
+from fabric.contrib.console import confirm
+from contextlib import contextmanager as _contextmanager
+
+
+
+ # Default release is 'current'
+env.release = 'current'
+
+rsync_excludes = ("*_local.py", "*.pyc", ".git/*", "*node_modules*", "composer.phar", "adminer.php", "dev/*", "fonts/*", "img/*", "less/*", "package/*", "templates/*")
+
+
+def production():
+	env.settings = 'production'
+	env.user = 'razorflow'
+	env.hosts = ['tools.razorflow.com']
+ 
+def deploy():
+	upload_app ()
+	print(green("##################################################################"))
+	print(green("## Finished deploy without any known errors. Please still test. ##"))
+	print(green("##################################################################"))
+
+def upload_app ():
+	rsync_project(
+		remote_dir= "/home/razorflow/jsrf_customBuilder/",
+		local_dir="src",
+		exclude=rsync_excludes,
+	)
