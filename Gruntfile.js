b32ade07ec84e8d7a7e494007b133c6c4c508cb5
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2014-07-22 15:17:06 +0530

    changes for v1final

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,13 +59,75 @@
                     apiTemplates: "src/templates/default/api_templates"
                 }
             },
-            website: {
+            // website: {
+            //     options: {
+            //         articles: {
+            //             root: 'src/content',
+            //             imagesLocalPath: 'src/images',
+            //             imagesRelativePath: "dashboard/_images/",
+            //             imagesPhysicalPath: "../newsite/src/docs/dashboard/_images/"
+            //         },
+            //         constantsPath: '../jsrf/tools/config/props.json',
+            //         partialsPath: 'src/partials',
+            //         examples: {
+            //             'js': {
+            //                 src: "../examples/src/js/examples/",
+            //                 srcSuffix: ".js",
+            //                 imagePrefix: "http://examples.razorflow.com/static/exampleImages/js/examples/",
+            //                 imageSuffix: ".png",
+            //                 thumbPrefix: "http://examples.razorflow.com/static/exampleImages/js/examples/",
+            //                 thumbSuffix: ".png",
+            //                 livePrefix: "http://examples.razorflow.com/dashboard/js/examples/",
+            //                 liveSuffix: ""
+            //             },
+            //             'php': {
+            //                 src: "../examples/src/php/examples/",
+            //                 srcSuffix: ".php",
+            //                 imagePrefix: "http://examples.razorflow.com/static/exampleImages/php/examples/",
+            //                 imageSuffix: ".png",
+            //                 thumbPrefix: "http://examples.razorflow.com/static/exampleImages/php/examples/",
+            //                 thumbSuffix: ".png",
+            //                 livePrefix: "http://examples.razorflow.com/dashboard/php/examples/",
+            //                 liveSuffix: ""
+            //             }
+            //         },
+            //         api: {
+            //             "relativeLinkPath": 'api',
+            //             lang: {
+            //                 "js": {
+            //                     src: ["../jsrf/src/js/components/*.js"],
+            //                     out: "../newsite/src/docs/dashboard/api/js/",
+            //                     relativeLinkPath: "api/js/"
+            //                 },
+            //                 "php": {
+            //                     src: ["../phprf/src/lib/components/*.php", "../phprf/src/lib/util/*.php"],
+            //                     out: "../newsite/src/docs/dashboard/api/php/",
+            //                     relativeLinkPath: "api/php/"
+            //                 }
+            //             }
+            //         },
+            //         linkPrefix: "/docs",
+            //         out: "../newsite/src/docs/dashboard/",
+            //         suffix: "php",
+            //         /*
+            //         base.ejs
+            //         article.ejs
+            //         api.ejs
+
+            //         ... all other remaining EJS files
+            //          */
+            //         articleTemplates: "src/templates/razorflow_dotcom/article_templates",
+            //         apiTemplates: "src/templates/razorflow_dotcom/api_templates"
+            //     }
+            // }
+            // 
+            website_js: {
                 options: {
                     articles: {
-                        root: 'src/content',
+                        root: 'src/content/js',
                         imagesLocalPath: 'src/images',
-                        imagesRelativePath: "dashboard/_images/",
-                        imagesPhysicalPath: "../newsite/src/docs/dashboard/_images/"
+                        imagesRelativePath: "js/_images/",
+                        imagesPhysicalPath: "../newsite/src/docs/js/_images/"
                     },
                     constantsPath: '../jsrf/tools/config/props.json',
                     partialsPath: 'src/partials',
@@ -80,6 +142,67 @@
                             livePrefix: "http://examples.razorflow.com/dashboard/js/examples/",
                             liveSuffix: ""
                         },
+                        // 'php': {
+                        //     src: "../examples/src/php/examples/",
+                        //     srcSuffix: ".php",
+                        //     imagePrefix: "http://examples.razorflow.com/static/exampleImages/php/examples/",
+                        //     imageSuffix: ".png",
+                        //     thumbPrefix: "http://examples.razorflow.com/static/exampleImages/php/examples/",
+                        //     thumbSuffix: ".png",
+                        //     livePrefix: "http://examples.razorflow.com/dashboard/php/examples/",
+                        //     liveSuffix: ""
+                        // }
+                    },
+                    api: {
+                        "relativeLinkPath": 'api',
+                        lang: {
+                            "js": {
+                                src: ["../jsrf/src/js/components/*.js"],
+                                out: "../newsite/src/docs/js/api",
+                                relativeLinkPath: "api/"
+                            },
+                            // "php": {
+                            //     src: ["../phprf/src/lib/components/*.php", "../phprf/src/lib/util/*.php"],
+                            //     out: "../newsite/src/docs/dashboard/api/php/",
+                            //     relativeLinkPath: "api/php/"
+                            // }
+                        }
+                    },
+                    linkPrefix: "/docs",
+                    out: "../newsite/src/docs/js/",
+                    suffix: "php",
+                    /*
+                    base.ejs
+                    article.ejs
+                    api.ejs
+
+                    ... all other remaining EJS files
+                     */
+                    articleTemplates: "src/templates/razorflow_dotcom/article_templates",
+                    apiTemplates: "src/templates/razorflow_dotcom/api_templates"
+                }
+            },
+            website_php: {
+                options: {
+                    articles: {
+                        root: 'src/content/php',
+                        imagesLocalPath: 'src/images',
+                        imagesRelativePath: "php/_images/",
+                        imagesPhysicalPath: "../newsite/src/docs/php/_images/"
+                    },
+                    constantsPath: '../jsrf/tools/config/props.json',
+                    partialsPath: 'src/partials',
+                    examples: {
+                        // 'js': {
+                        //     src: "../examples/src/js/examples/",
+                        //     srcSuffix: ".js",
+                        //     imagePrefix: "http://examples.razorflow.com/static/exampleImages/js/examples/",
+                        //     imageSuffix: ".png",
+                        //     thumbPrefix: "http://examples.razorflow.com/static/exampleImages/js/examples/",
+                        //     thumbSuffix: ".png",
+                        //     livePrefix: "http://examples.razorflow.com/dashboard/js/examples/",
+                        //     liveSuffix: ""
+                        // },
                         'php': {
                             src: "../examples/src/php/examples/",
                             srcSuffix: ".php",
@@ -94,20 +217,20 @@
                     api: {
                         "relativeLinkPath": 'api',
                         lang: {
-                            "js": {
-                                src: ["../jsrf/src/js/components/*.js"],
-                                out: "../newsite/src/docs/dashboard/api/js/",
-                                relativeLinkPath: "api/js/"
-                            },
+                            // "js": {
+                            //     src: ["../jsrf/src/js/components/*.js"],
+                            //     out: "../newsite/src/docs/dashboard/api/js/",
+                            //     relativeLinkPath: "api/js/"
+                            // },
                             "php": {
                                 src: ["../phprf/src/lib/components/*.php", "../phprf/src/lib/util/*.php"],
-                                out: "../newsite/src/docs/dashboard/api/php/",
-                                relativeLinkPath: "api/php/"
+                                out: "../newsite/src/docs/php/api",
+                                relativeLinkPath: "api/"
                             }
                         }
                     },
                     linkPrefix: "/docs",
-                    out: "../newsite/src/docs/dashboard/",
+                    out: "../newsite/src/docs/php/",
                     suffix: "php",
                     /*
                     base.ejs
