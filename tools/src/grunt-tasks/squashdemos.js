8d58ab6dbcde70c3b6f2acef166ef18b1a821bfc
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2014-07-22 14:51:05 +0530

    Added old squashdemos code as comments in case we need it

--- tools/src/grunt-tasks/squashdemos.js
+++ tools/src/grunt-tasks/squashdemos.js
@@ -43,55 +43,6 @@ module.exports = function(grunt) {
     }
 
     var demosJson = JSON.stringify(demosObject, null, '\n');
-
-
-    // var files = grunt.file.expand(options.demos);
-    // var phpFiles = grunt.file.expand(options.phpDemos);
-    // // console.log(phpFiles);
-    // for(var i = 0; i < files.length; i++) {
-    //   var item = files[i];
-    //   var id = path.basename (item, ".js");
-    //   var content = grunt.file.read(item);
-    //   var phpContent = '';
-    //   var phpFilename = _.filter(phpFiles, function(fn) { return fn.match(id); })[0];
-    //   if(phpFilename) {
-    //     phpContent = grunt.file.read(phpFilename);
-    //   }
-    //   var meta = JSON.parse(content.match(/\/\*&([^\*\/]*)/)[1]);
-
-    //   meta.content = content;
-    //   meta.phpContent = phpContent;
-    //   meta.method = "**content**"
-    //   if(mainObject[meta.section.id]) {
-    //     mainObject[meta.section.id].items[meta.id] = meta;
-    //   } else {
-    //     mainObject[meta.section.id] = {
-    //       title: meta.section.title,
-    //       items: {}
-    //     }
-    //     mainObject[meta.section.id].items[meta.id] = meta;
-    //   }
-    // }
-
-    // for(var key in mainObject) {
-    //   out += '"' + key + '" : { \n';
-    //   out += '"title": "' + mainObject[key].title + '",\n';
-    //   out += '"items": {\n';
-    //   var items = mainObject[key].items;
-    //   for(var item in items) {
-    //     var content = items[item].content;
-    //     var json = JSON.stringify(items[item]);
-    //     content = content.replace(/'/g, '"');
-    //     json = json.replace('"**content**"', "function() {" + content + "},");
-    //     out += '"' + item + '": ' + json + ",";
-    //   }
-    //   out += '}},';
-
-    // } 
-
-    // // console.log(out);
-
-    // out += "}";
     demoCode += '};';
     grunt.file.write(options.out, 'window.rfDemos = ' + demosJson + ';');
     grunt.file.write(options.outCode, demoCode);
@@ -100,3 +51,38 @@ module.exports = function(grunt) {
   });
 
 };
+
+// OLD squashdemos below in case we need it. Just comment everything above and uncomment everything below
+
+// var _ = require('underscore');
+// var fs = require('fs');
+// var path = require ('path');
+// module.exports = function(grunt) {
+
+//   // Please see the Grunt documentation for more information regarding task
+//   // creation: http://gruntjs.com/creating-tasks
+
+//   grunt.registerTask('squashdemos', 'Squash Demos', function() {
+//     var out = "window.rfDemos = {\n";
+//     grunt.log.writeln("Squashing demos...");
+
+//     // Merge task-specific and/or target-specific options with these defaults.
+//     var options = this.options({
+//     });
+
+//     var files = grunt.file.expand(options.demos);
+//     for(var i = 0; i < files.length; i++) {
+//       var item = files[i];
+//       var id = path.basename (item, ".js");
+//       var content = grunt.file.read(item);
+
+//       out += ""+id+":function () {\n" + content + "\n},\n";
+//     }
+
+//     out += "a:0}";
+//     grunt.file.write(options.out, out);
+
+//     grunt.log.ok("Finished squashing.");
+//   });
+
+// };
