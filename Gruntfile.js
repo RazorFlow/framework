38fc3514b0211c3176962df0b7fd7d586ec5b100
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2014-07-25 12:33:11 +0530

    php min and src package

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,22 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         copyto: {
+            srcToPackageMin: {
+                files: [
+                    {cwd: 'packages/minified/', src:['**/*'], dest: 'build/packages/minified/'},
+                    {cwd: 'src/', src: ['**'], dest: 'build/packages/minified/razorflow_php/'},
+                    {cwd: '../jsrf/build/assets/', src: ['js/*.js', 'css/*.css', 'fonts/**', 'img/**'], dest: 'build/packages/minified/razorflow_php/static/rf/'},
+                ]
+            },
+            srcToPackageSrc: {
+                files: [
+                    {cwd: 'packages/source/', src:['**/*'], dest: 'build/packages/source/'},
+                    {cwd: 'src/', src: ['**'], dest: 'build/packages/source/razorflow_php/'},
+                    {cwd: '../jsrf/build/assets/', src: ['js/*.js', 'css/*.css', 'fonts/**', 'img/**'], dest: 'build/packages/source/razorflow_php/static/rf/'},
+                    {cwd: '../jsrf/build/packages/source/source/', src: ['**/*'], dest: 'build/packages/source/source/'},
+                    {cwd: 'src/', src: ['**'], dest: 'build/packages/source/source/php/src/'}
+                ]
+            },
             assetsToPackage: {
                 files: [
                     {cwd: '../jsrf/build/assets/', src: ['js/*.js', 'css/*.css', 'fonts/**', 'img/**'], dest: 'build/package/static/rf/'},
@@ -13,12 +29,14 @@ module.exports = function (grunt) {
             }
         },
         clean: {
-            build: ["build/package"]
+            build: ["build/packages"]
         }
     });
 
     grunt.loadNpmTasks('grunt-copy-to');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('package', ["clean:build", 'copyto:libToPackage', 'copyto:assetsToPackage'])
+    grunt.registerTask('package', ["clean:build", 'copyto:libToPackage', 'copyto:assetsToPackage']);
+    grunt.registerTask('packageMin', ['clean:build', 'copyto:srcToPackageMin']);
+    grunt.registerTask('packageSrc', ['clean:build', 'copyto:srcToPackageSrc']);
 }
