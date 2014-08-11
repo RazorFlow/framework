d2731970d933b2e7897ba3049884ee78dbefdc57
Anirudh Sanjeev <anirudh@razorflow.com>
2014-08-11 11:27:36 +0530

    added setup file

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+npm install
+cd jsrf
+npm install
+cd ../examples/internal
+npm install
+cd ../../examples/apps/
+npm install
+cd ../../grunt-contrib-jst/
+npm install
+cd ../newsite/src/static/
+npm install
+cd ../../../phprf
+npm install
+cd ../razorcharts
+npm install
+cd ../docs/
+npm install
+cd ../razordoc/
+npm install
