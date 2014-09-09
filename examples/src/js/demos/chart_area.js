dba30ca9ac5c7b3e89c1e9787ecc2eb725abc400
narendrashetty <narendra@razorflow.com>
2014-09-09 13:09:44 +0530

    changed area chart DV RF-94 #resolve

--- examples/src/js/demos/chart_area.js
+++ examples/src/js/demos/chart_area.js
@@ -1,11 +1,11 @@
 rf.StandaloneDashboard(function(db){
     var chart = new ChartComponent();
     chart.setDimensions (8, 6);
-    chart.setCaption("Visits by Month");
+    chart.setCaption("Number of monthly unique visitors on a website in 2013");
     chart.setLabels (["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]);
     chart.addSeries ("month_2013", "2013", [420000, 460000, 480000, 520000, 560000, 510000, 470000, 430000, 420000, 370000, 360000, 360000], {
         seriesDisplayType: "area"
     });
-    chart.setYAxis('', {numberPrefix: '$', numberHumanize: true});
+    chart.setYAxis('Number of visitors', {numberHumanize: true});
     db.addComponent (chart);
 });
