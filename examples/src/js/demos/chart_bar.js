da04fcb349ec0b6c30ea23f3587fd21d88124192
narendrashetty <narendra@razorflow.com>
2014-09-09 13:02:56 +0530

    changed v to vs in feature demo RF-93 #resolve

--- examples/src/js/demos/chart_bar.js
+++ examples/src/js/demos/chart_bar.js
@@ -1,6 +1,6 @@
 StandaloneDashboard(function (db) {
     var c1 = new ChartComponent();
-    c1.setCaption("Costs by division - 2013 v 2012");
+    c1.setCaption("Costs by division - 2013 vs 2012");
     c1.setDimensions(8, 6);
     c1.setLabels(['Manufacturing', 'Publishing', 'Transportation', 'Communications']);
     c1.addSeries("costs2013", "2013", [24400, 27800, 23800, 24800], {seriesDisplayType: 'bar'});
--- examples/src/js/demos/chart_line.js
+++ examples/src/js/demos/chart_line.js
@@ -1,7 +1,7 @@
 rf.StandaloneDashboard(function(db){
 	var chart = new ChartComponent('sales');
 	chart.setDimensions (8, 6);
-	chart.setCaption("Sales - 2013 v 2012");
+	chart.setCaption("Sales - 2013 vs 2012");
 	chart.setLabels (["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]);
 	chart.addSeries ("sales2013", "2013", [22400, 24800, 21800, 21800, 24600, 27600, 26800, 27700, 23700, 25900, 26800, 24800], {
 		seriesDisplayType: "line"
