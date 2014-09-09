fe6a66044a3e8bdc2d2b34d3bc25cfa42012e08b
narendrashetty <narendra@razorflow.com>
2014-09-09 13:29:16 +0530

    fixed feature page simple kpi RF-101 #resolve

--- examples/src/js/demos/kpi_simple.js
+++ examples/src/js/demos/kpi_simple.js
@@ -1,7 +1,7 @@
 rf.StandaloneDashboard(function(db){
     var kpi = new KPIComponent ();
     kpi.setDimensions (3, 2);
-    kpi.setCaption ('Sales in 24h');
+    kpi.setCaption ('Sales in the last 24 hours');
     kpi.setValue (3145, {
         numberPrefix: "$"
     });
